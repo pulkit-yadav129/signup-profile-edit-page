c49899611fa64d7e2e46b90511cb3b5fe4873daa
Pulkit Yadav <96831141+pulkit-yadav129@users.noreply.github.com>
2023-06-22 15:31:20 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+//import firebase from 'firebase';
+//import {getAuth} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKt-z3MOyBtROxkfiPQoGfqGNKF1I-AfI",
+    authDomain: "fitness-app-83441.firebaseapp.com",
+    projectId: "fitness-app-83441",
+    storageBucket: "fitness-app-83441.appspot.com",
+    databaseURL: "https://fitness-app-83441-default-rtdb.firebaseio.com",
+    messagingSenderId: "814056889538",
+    appId: "1:814056889538:web:02e18a609bcb8ba6cdaf5b",
+    measurementId: "G-3CXP3YJKEP"
+};
+
+
